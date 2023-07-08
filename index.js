5e2876af3de8a87b09cdcdcc07ef47a51d59ef01
Frying Lotus <blightningboltz5@gmail.com>
2023-07-08 23:53:14 +0700

    Finalize the change

--- index.js
+++ index.js
@@ -1,19 +1,3 @@
-// let time = document.getElementById("current-time");
-
-// setInterval(() => {
-//     let d = new Date();
-//     time.innerHTML = d.toLocaleTimeString();
-// }, 1000);
-
-// let field = document.querySelector('#date');
-
-// // Handle date changes
-// date.addEventListener('input', function () {
-// 	// ...
-// });
-
-
-
 
 function calcSvayRieng()
 {   
@@ -22,27 +6,27 @@ function calcSvayRieng()
         //     x = parseFloat(o).toFixed(2);
          day = document.getElementById("days").value;
          month = document.getElementById("months").value;
+         year = document.getElementById("years").value;
          getMonth = month * 30.417;
          total = day + getMonth;
          u = (2*3.14*(total-104)/365);
           o = 39.2 * (Math.cos(u)) + 727.6;
             total = parseFloat(o).toFixed(2);
-         if (month > 12){
-          document.getElementById("result").innerHTML = "Month too high";
+         if (month > 12 || month < 0){
+          document.getElementById("result").innerHTML = "Month too high or too low";
          } else if (month == 2 && day >= 29){
           document.getElementById("result").innerHTML = "February has only 28 days";
+         }else if (day > 31 || day < 0){
+            document.getElementById("result").innerHTML = "Day is too high or too low";
          } else if(day == 0 && month == 0){
             document.getElementById("result").innerHTML = "No input";
+         }else if (year >= 3000 || year < 0 ){
+          document.getElementById("result").innerHTML = "Years too high or too low."; 
+         
            }else{
             document.getElementById("result").innerHTML = total + " Minutes ";
           }
-       
-
-         
-        
-            
-        
-        
+      
 }
 function calcDayBanteay(){
     day = document.getElementById("days").value;
@@ -52,15 +36,21 @@ function calcDayBanteay(){
     u = (2*3.14*(total-172)/365);
     o = 48.65 * (Math.cos(u)) + 727.65;
     total = parseFloat(o).toFixed(2);
-    if (month > 12){
-    document.getElementById("result").innerHTML = "Month too high";
-    } else if (month == 2 && day >= 29){
-    document.getElementById("result").innerHTML = "February has only 28 days";
-    } else if(day == 0 && month == 0){
-    document.getElementById("result").innerHTML = "No input";
-    }else{
-    document.getElementById("result").innerHTML = total + " Minutes ";
-  }
+    if (month > 12 || month < 0){
+      document.getElementById("result").innerHTML = "Month too high or too low";
+     } else if (month == 2 && day >= 29){
+      document.getElementById("result").innerHTML = "February has only 28 days";
+     }else if (day > 31 || day < 0){
+        document.getElementById("result").innerHTML = "Day is too high or too low";
+     } else if(day == 0 && month == 0){
+        document.getElementById("result").innerHTML = "No input";
+     }else if (year >= 3000 || year < 0 ){
+      document.getElementById("result").innerHTML = "Years too high or too low."; 
+     
+       }else{
+        document.getElementById("result").innerHTML = total + " Minutes ";
+      }
+   
 }
 function calcDayLaos() {
  day = document.getElementById("days").value;
@@ -71,14 +61,20 @@ function calcDayLaos() {
 
     o = 69.7 * (Math.cos(u)) + 732.9;
       total = parseFloat(o).toFixed(2);
-      if (month > 12){
-        document.getElementById("result").innerHTML = "Month too high";
+      if (month > 12 || month < 0){
+        document.getElementById("result").innerHTML = "Month too high or too low";
        } else if (month == 2 && day >= 29){
         document.getElementById("result").innerHTML = "February has only 28 days";
+       }else if (day > 31 || day < 0){
+          document.getElementById("result").innerHTML = "Day is too high or too low";
        } else if(day == 0 && month == 0){
           document.getElementById("result").innerHTML = "No input";
+       }else if (year >= 3000 || year < 0 ){
+        document.getElementById("result").innerHTML = "Years too high or too low."; 
+       
          }else{
           document.getElementById("result").innerHTML = total + " Minutes ";
         }
+     
      
 }
